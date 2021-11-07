14089bac75a5145ed76dac413a68eeab985140f2
aac-devs <aac.devs@gmail.com>
2021-11-07 12:00:43 -0300

    up to Service 1: /ping (S4-V13) video

--- index.js
+++ index.js
@@ -106,10 +106,10 @@ var unifiedServer = function(req, res){
 // Define the handlers
 var handlers = {}
 
-// Sample handler
-handlers.sample = function(data, callback){
+// Ping handler
+handlers.ping = function(data, callback){
   // Callback a http status code, and payload object
-  callback(406, { 'name': 'sample handler'});
+  callback(200);
 };
 
 // Not found handler
@@ -119,5 +119,5 @@ handlers.notFound = function(data, callback){
 
 // Define a request router
 var router = {
-  'sample': handlers.sample
+  'ping': handlers.ping
 };
