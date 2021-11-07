da95ac65ae32a34f789eaa2772faf0085b0989d5
aac-devs <aac.devs@gmail.com>
2021-11-07 11:55:28 -0300

    up to Adding HTTPS Support (S4-V12) video

--- index.js
+++ index.js
@@ -5,12 +5,39 @@
 
 // Dependencies
 var http = require('http');
+var https = require('https');
 var url = require('url');
 var StringDecoder = require('string_decoder').StringDecoder;
 var config = require('./config');
+var fs = require('fs');
 
-// The server should respond to all requests with a string
-var server = http.createServer(function(req, res){
+// Instatiate the HTTP server
+var httpServer = http.createServer(function(req, res){
+  unifiedServer(req, res);
+});
+
+// Start the HTTP server
+httpServer.listen(config.httpPort, function(){
+  console.log(`The server is listening on port ${config.httpPort} in ${config.envName} mode`);
+});
+
+// Instantiate the HTTPS server
+var httpsServerOptions = {
+  'key': fs.readFileSync('./https/key.pem'),
+  'cert': fs.readFileSync('./https/cert.pem')
+};
+
+var httpsServer = https.createServer(httpsServerOptions, function(req, res){
+  unifiedServer(req, res);
+});
+
+// Start the HTTPS server
+httpsServer.listen(config.httpsPort, function(){
+  console.log(`The server is listening on port ${config.httpsPort} in ${config.envName} mode`);
+});
+
+// All the server logic for both the http and https server
+var unifiedServer = function(req, res){
   console.clear();
   // Get the URL and parse it
   var parsedUrl = url.parse(req.url, true)
@@ -72,13 +99,9 @@ var server = http.createServer(function(req, res){
       // Log the request path
       console.log('Returning this response:', statusCode, payloadString);
     });
-  })
-});
+  });
+};
 
-// Start the server
-server.listen(config.port, function(){
-  console.log(`The server is listening on port ${config.port} in ${config.envName} mode`);
-});
 
 // Define the handlers
 var handlers = {}
